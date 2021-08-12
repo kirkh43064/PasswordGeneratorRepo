ec408b906c92703e0f7b75f03cb78a7ee152728a
Kirk Hallam <kirkh43064@gmail.com>
2021-08-12 16:58:51 -0400

    Creaed variables and arrays, also started function to pick choices

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// create the arrays and variables
+const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var passwordLength;
+var confirmLower;
+var confirmUpper;
+var confirmNumber;
+var confirmSpecial;
+var userChoices;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +25,111 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Start function
+function generatePassword() {
+  // get user input
+  passwordLength = prompt("How many character would you like in your password? Choose between 8 and 128")
+  console.log("Password length = " + passwordLength);
+
+  if(!passwordLength) {
+    alert("Required Value!");
+  }
+
+  else if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("you must choose between 8 and 128";)
+  }
+
+  else {
+    confirmLower = confirm("Do you want lower case letters?");
+    console.log("Lower case = " + confirmLower);
+    confirmUpper = confirm("Do you want Upper Case letters?");
+    console.log("Upper case = " + confirmUpper);
+    confirmNumber = confirm("Do you want numbers?");
+    console.log("Numbers = " + confirmNumber);
+    confirmSpecial = confirm("Do you want special character?");
+    console.log("Special Characters = " + confirmSpecial);
+  }
+
+  //if no confirmation
+  if (!confirmLower && !confirmUpper && !confirmNumber && !confirmSpecial) {
+    userChoices = alert("You must choose at least one criteria");
+  }
+
+  // for 4 true answers (they picked all 4)
+  else if (confirmLower && confirmUpper && confirmNumber && confirmSpecial) {
+    userChoices = (lower, upper, number, special);
+    console.log(userChoices);
+  }
+
+  // for 3 true (they picked 3)
+  else if ( confirmLower && confirmUpper && confirmNumber) {
+    userChoices = (lower, upper, number);
+    console.log(userChoices);
+  }
+
+  else if (confirmLower && confirmUppe && confirmSpecial) {
+    userChoices = (lower, upper, special);
+    console.log(userChoices);
+  }
+
+  else if (confirmLower && confirmNumber && confirmSpecial) {
+    userChoices = (lower, upper, special);
+    console.log(userChoices);
+  }
+
+  else if (confirmNumber && confirmUpper && confirmSpecial) {
+    userChoices = (lower, upper, special);
+    console.log(userChoices);
+  }
+
+  // for 2 true answers (they picked 2)
+  else if (confirmLower && confirmUpper) {
+    userChoices = (lower, upper);
+    console.log(userChoices);
+  }
+
+  else if (confirmLower && confirmSpecial) {
+    userChoices = (lower, special);
+    console.log(userChoices);
+  }
+
+  else if (confirmLower && confirmNumber) {
+    userChoices = (lower, upper);
+    console.log(userChoices);
+  }
+
+  else if (confirmNumber && confirmSpecial) {
+    userChoices = (number, special);
+    console.log(userChoices);
+  }
+
+  else if (confirmSpecial && confirmUpper) {
+    userChoices = (special, upper);
+    console.log(userChoices);
+  }
+
+  else if (confirmNumber && confirmUpper) {
+    userChoices = (number, upper);
+    console.log(userChoices);
+  }
+
+  // 1 true (they only picked 1)
+  else if (confirmLower) {
+    userChoices = lower;
+  }
+
+  else if (confirmUpper) {
+    userChoices = upper;
+  }
+
+  else if (confirmNumber) {
+    userChoices = number;
+  }
+
+  else if (confirmSpecial) {
+    userChoices = special;
+  }
+
+}
+
