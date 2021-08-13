b2d6ffaa0446aa03c13efc06ebd13634b720096f
Kirk Hallam <kirkh43064@gmail.com>
2021-08-13 18:47:05 -0400

    completely changed the code got rid of old ugly code and got new cleaner code

--- Develop/script.js
+++ Develop/script.js
@@ -1,150 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// create the arrays and variables
-const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-let passwordLength;
-let confirmLower;
-let confirmUpper;
-let confirmNumber;
-let confirmSpecial;
-let userChoices;
-
-// Write password to the #password input
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
+
+// create the arrays and variables
+const number = [1,2,3,4,5,6,7,8,9,0];
+const special = ["!", "@", "#", "$", "^", "&", "%", "*", "(", ")", "+", "=", "-", "[", "]", "{", "}", "|", ":", "<", ">", "?", ",",];
+const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+let pwdArray = [];
+let randomPass = "";
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Start function
-function generatePassword() {
-  // get user input
-  passwordLength = prompt("How many character would you like in your password? Choose between 8 and 128")
-  console.log("Password length = " + passwordLength);
-
-  if(!passwordLength) {
-    alert("Required Value!");
+function writePassword() {
+  let pwLength = prompt("Please enter the number of characters you want for you new password. It may be between 8 and 128 characters.");
+  
+  if (pwLength <= 7 || pwLength >= 129) {
+    return alert("Error: you must choose between 8 and 128")
   }
 
-  else if (passwordLength < 8 || passwordLength > 128) {
-    passwordLength = prompt("you must choose between 8 and 128");
+  let useLower = confirm("Do you want your new password to include lowercase letters?");
+    if (useLower === true) {
+    pwdArray = pwdArray.concat(lower);
   }
 
-  else {
-    confirmLower = confirm("Do you want lower case letters?");
-    console.log("Lower case = " + confirmLower);
-    confirmUpper = confirm("Do you want Upper Case letters?");
-    console.log("Upper case = " + confirmUpper);
-    confirmNumber = confirm("Do you want numbers?");
-    console.log("Numbers = " + confirmNumber);
-    confirmSpecial = confirm("Do you want special character?");
-    console.log("Special Characters = " + confirmSpecial);
+  let useUpper = confirm("Do you want your new password to include uppercase letters?");
+    if (useUpper === true) {
+    pwdArray = pwdArray.concat(upper);
   }
 
-  //if no confirmation
-  if (!confirmLower && !confirmUpper && !confirmNumber && !confirmSpecial) {
-    userChoices = alert("You must choose at least one criteria");
+  let useSpecial = confirm("Do you want your new password to include special character?");
+    if (useSpecial === true) {
+    pwdArray = pwdArray.concat(special);
   }
 
-  // for 4 true answers (they picked all 4)
-  else if (confirmLower && confirmUpper && confirmNumber && confirmSpecial) {
-    userChoices = (lower, upper, number, special);
-    console.log(userChoices);
+  let useNumb = confirm("Do you want your new password to include numbers?");
+    if (useNumb === true) {
+    pwdArray = pwdArray.concat(number);
   }
 
-  // for 3 true (they picked 3)
-  else if ( confirmLower && confirmUpper && confirmNumber) {
-    userChoices = (lower, upper, number);
-    console.log(userChoices);
+  for (let i = 0; i < pwLength; i++) {
+    let generate = pwdArray[Math.floor(Math.random() * (pwdArray.length))];
+    password += generate
+    console.log(generate);
   }
 
-  else if (confirmLower && confirmUppe && confirmSpecial) {
-    userChoices = (lower, upper, special);
-    console.log(userChoices);
-  }
-
-  else if (confirmLower && confirmNumber && confirmSpecial) {
-    userChoices = (lower, upper, special);
-    console.log(userChoices);
-  }
-
-  else if (confirmNumber && confirmUpper && confirmSpecial) {
-    userChoices = (lower, upper, special);
-    console.log(userChoices);
-  }
-
-  // for 2 true answers (they picked 2)
-  else if (confirmLower && confirmUpper) {
-    userChoices = (lower, upper);
-    console.log(userChoices);
-  }
-
-  else if (confirmLower && confirmSpecial) {
-    userChoices = (lower, special);
-    console.log(userChoices);
-  }
-
-  else if (confirmLower && confirmNumber) {
-    userChoices = (lower, upper);
-    console.log(userChoices);
-  }
-
-  else if (confirmNumber && confirmSpecial) {
-    userChoices = (number, special);
-    console.log(userChoices);
-  }
-
-  else if (confirmSpecial && confirmUpper) {
-    userChoices = (special, upper);
-    console.log(userChoices);
-  }
-
-  else if (confirmNumber && confirmUpper) {
-    userChoices = (number, upper);
-    console.log(userChoices);
-  }
-
-  // 1 true (they only picked 1)
-  else if (confirmLower) {
-    userChoices = lower;
-  }
-
-  else if (confirmUpper) {
-    userChoices = upper;
-  }
-
-  else if (confirmNumber) {
-    userChoices = number;
-  }
-
-  else if (confirmSpecial) {
-    userChoices = special;
-  }
-
+  document.getElementById("password").innerHTML = password;
+return password;
 }
-
-// create variable for password length
-let passwordBlank = [];
-
-// run loop for each random item
-for (let i = 0; i < passwordLength; i++) {
-  let finalChoices = userChoices[Math.floor(Math.random() * userChoices.length)];
-  passwordBlank.push(finalChoices);
-  console.log(finalChoices);
-}
-
-// combine and return password
-let password = passwordBlank.join("");
-console.log("our password is: " + password);
-
-document.getElementById("password").innerHTML = passwordBlank;
